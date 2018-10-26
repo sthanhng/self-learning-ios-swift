57a597445236bf2e6cc3fc5281c703823973bee1
ThanhNS <sthanhng@gmail.com>
2018-10-26 23:11:07 +0700

    add Operators

---
+++ hacking-swift/Operators.playground/Contents.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+var str = "Hello, playground"
+
+/*
+ * Let's try a few basics - please type this into your playground
+ */
+var a = 10
+a = a + 1
+a = a - 4
+a = a * a
+
+// In the result pane, you'll see 10, 11, 7, and 49 respectively.
+// Now try this
+var b = 26
+b += 10
+b -= 9
+
+// When it comes to strings, + will join them together. For example:
+var name1 = "Thanh Nguyen"
+var name2 = "Nguyen Thanh"
+var both = name1 + " and " + name2
+
+// That will write "Thanh Nguyen and Nguyen Thanh" into the result pane
