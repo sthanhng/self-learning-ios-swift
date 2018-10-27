ffb3ed222f34ecf088cc139d10cd11905923affb
ThanhNS <sthanhng@gmail.com>
2018-10-27 20:39:33 +0700

    add Dictionaries tut

---
+++ hacking-swift/Dictonaries.playground/Contents.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+var str = "Hello, playground"
+
+/*
+ * Dictinaries are another common type of collection, but they differ from arrays
+ * because thay let you access values based on a key you specify.
+ */
+var person = [
+    "first": "Thanh",
+    "middle": "Sanh",
+    "last": "Nguyen",
+    "month": "February",
+    "website": "sthanhng.github.io"
+]
+
+person["middle"]
+person["last"]
+person["website"]
